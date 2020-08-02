f5a49f1741080dc73be63d379552fccc4b8be821
ParkHyeonJae <md101106@gmail.com>
2020-08-02 18:32:13 +0900

    feat: Smart_Pointer_shared_ptr ex02

--- Coding/TEST/TEST/Smart_Pointer.cpp
+++ Coding/TEST/TEST/Smart_Pointer.cpp
@@ -3,20 +3,43 @@
 
 using namespace std;
 
+class Person
+{
+private:
+	string name;
+	int age;
+public:
+	Person(const string& name, int age);		//기초 클래스 생성자의 선언
+	~Person() { cout << "소멸자가 호출되었습니다." << endl; }
+
+	void ShowPersonInfo();
+};
+
+
 int main()
 {
-	// int 형 shared_ptr인 ptr01을 선언하고 초기화함
-	shared_ptr<int> ptr01(new int(5));
+	shared_ptr<Person> hong = make_shared<Person>("박현재", 18);
+	cout << "현재 소유자 수 : " << hong.use_count() << endl;
 
-	//use_count()는 참조중인 소유자의 수를 반환해 준다
-	cout << ptr01.use_count() << endl;		// 1개
-	auto ptr02(ptr01);
-	cout << ptr01.use_count() << endl;		// 2개
-	auto ptr03 = ptr01;
-	auto ptr04 = ptr01;
-	auto ptr05 = ptr01;
-	cout << ptr01.use_count() << endl;		//5개
+	auto han = hong;
 
+	cout << "현재 소유자 수 : " << hong.use_count() << endl;
+
+	han.reset();
+
+	cout << "현재 소유자 수 : " << hong.use_count() << endl;
 
 	return 0;
+}
+
+Person::Person(const string& name, int age)
+{
+	this->name = name;
+	this->age = age;
+	cout << "생성자가 호출되었습니다." << endl;
+}
+
+void Person::ShowPersonInfo()
+{
+	cout << name << "의 나이는 " << age << "살입니다." << endl;
 }
