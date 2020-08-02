83af05e224efc46fc324b96acde57649bbdf0382
ParkHyeonJae <md101106@gmail.com>
2020-08-02 16:22:19 +0900

    feat: Smart_Pointer_Unique_ptr ex02

--- Coding/TEST/TEST/Smart_Pointer.cpp
+++ Coding/TEST/TEST/Smart_Pointer.cpp
@@ -1,15 +1,37 @@
-#include <stdio.h>
+#include <iostream>
 #include <memory>
 
 using namespace std;
 
+class Person
+{
+private:
+	string name;
+	int age;
+public:
+	Person(const string& name, int age);		//기초 클래스 생성자의 선언
+	~Person() { cout << "소멸자가 호출되었습니다." << endl; }
+
+	void ShowPersonInfo();
+};
+
+
 int main()
 {
-	//int 형 unique_ptr인 ptr01을 선언하고 초기화함
-	unique_ptr<int> ptr01(new int(5));
+	unique_ptr<Person> hong = make_unique<Person>("박현재", 18);
+	hong->ShowPersonInfo();
 
-	auto ptr02 = move(ptr01);
-	ptr02.reset();
-	ptr01.reset();
 	return 0;
+}
+
+Person::Person(const string& name, int age)
+{
+	this->name = name;
+	this->age = age;
+	cout << "생성자가 호출되었습니다." << endl;
+}
+
+void Person::ShowPersonInfo()
+{
+	cout << name << "의 나이는 " << age << "살입니다." << endl;
 }
