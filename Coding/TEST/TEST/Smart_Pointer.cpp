de1c2055b4f294134f9c3331d44ad769e8e474f0
ParkHyeonJae <md101106@gmail.com>
2020-08-02 18:18:08 +0900

    feat: Smart_Pointer_shared_ptr ex01

--- Coding/TEST/TEST/Smart_Pointer.cpp
+++ Coding/TEST/TEST/Smart_Pointer.cpp
@@ -3,40 +3,20 @@
 
 using namespace std;
 
-class Person
-{
-private:
-	string name;
-	int age;
-public:
-	Person(const string& name, int age);		//기초 클래스 생성자의 선언
-	~Person() { cout << "소멸자가 호출되었습니다." << endl; }
-
-	void ShowPersonInfo();
-};
-
-
 int main()
 {
-	/* Person 객체를 가리키는 unique_ptr인스턴스인 hong은 
-	일반 포인터와는 달리 사용이 끝난 후에 delete 키워드를 사용하여 메모리를 해제할 필요가 없다.
-	*/
-	unique_ptr<Person> hong = make_unique<Person>("박현재", 18);
-	hong->ShowPersonInfo();
+	// int 형 shared_ptr인 ptr01을 선언하고 초기화함
+	shared_ptr<int> ptr01(new int(5));
+
+	//use_count()는 참조중인 소유자의 수를 반환해 준다
+	cout << ptr01.use_count() << endl;		// 1개
+	auto ptr02(ptr01);
+	cout << ptr01.use_count() << endl;		// 2개
+	auto ptr03 = ptr01;
+	auto ptr04 = ptr01;
+	auto ptr05 = ptr01;
+	cout << ptr01.use_count() << endl;		//5개
+
 
 	return 0;
-}
-
-Person::Person(const string& name, int age)
-{
-	this->name = name;
-	this->age = age;
-	cout << "생성자가 호출되었습니다." << endl;
-
-
-}
-
-void Person::ShowPersonInfo()
-{
-	cout << name << "의 나이는 " << age << "살입니다." << endl;
 }
