f39238bcb069d1539922c9bdec15dc349ac6d02b
ParkHyeonJae <md101106@gmail.com>
2020-08-03 12:29:13 +0900

    feat: Server/prime_number_errorneous

--- Coding/GameServer/Study_GameServer/Study_GameServer/prime_number_errorneous.cpp
+++ Coding/GameServer/Study_GameServer/Study_GameServer/prime_number_errorneous.cpp
@@ -46,10 +46,36 @@ int main()
 	for (int i = 0; i < ThreadCount; i++)
 	{
 		shared_ptr<thread> thread(new thread([&]() {
+			while (true)
+			{
+				int n;
+				n = num;
+				num++;
 
+				if (n >= MaxCount)
+					break;
+
+				if (IsPrimeNumebr(n))
+				{
+					primes.push_back(n);
+					continue;
+				}
+			}
 			}));
+
+
+		threads.push_back(thread);
 	}
 
+	//모든 스레드가 일을 마칠 때까지 기다린다.
+	for (auto thread : threads)
+	{
+		thread->join();
+	}
 
+	auto t1 = chrono::system_clock::now();
+	
+	auto duration = chrono::duration_cast<chrono::milliseconds>(t1 - t0).count();
+	cout << "Took " << duration << " milliseconds." << endl;
 	return 0;
 }
--- Coding/TEST/TEST/Smart_Pointer.cpp
+++ Coding/TEST/TEST/Smart_Pointer.cpp
@@ -3,47 +3,8 @@
 
 using namespace std;
 
-class SimpleCat
-{
-public:
-	SimpleCat(int age, int weight);			// Constructor
-	SimpleCat(SimpleCat& simplecat);		//Copy constructor
-	~SimpleCat();		//destructor
-	int GetAge() const { return itsAge; }
-	void SetAge(int age) { itsAge = age; }
-private:
-	int itsAge;
-	int itsWeight;
-};
-SimpleCat::SimpleCat(int age, int weight) : itsAge(age), itsWeight(weight)
-{
-	cout << "Simple Cat Constructor..\n";
-}
-SimpleCat::SimpleCat(SimpleCat& simplecat)
-{
-	cout << "Simple Cat Copy Constructor...\n";
-}
-SimpleCat::~SimpleCat()
-{
-	cout << "Simple Cat Destructor..\n";
-}
-
-shared_ptr<SimpleCat> TheFunction();
-
 int main()
 {
-	auto rCat = TheFunction();
-	auto rat02 = rCat;
-	int age = rCat->GetAge();
-	cout << "rCat is " << age << " years old\n";
+
 	return 0;
-}
-
-
-shared_ptr<SimpleCat> TheFunction()
-{
-	shared_ptr<SimpleCat> frisky = make_shared<SimpleCat>(5, 9);
-	cout << "frisky is " << frisky->GetAge() << " years old\n";
-	return frisky;
-	
 }
