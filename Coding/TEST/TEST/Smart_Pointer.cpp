c5f54ddd8ec431fe0ee36c6b663c0baf9b6777e7
ParkHyeonJae <md101106@gmail.com>
2020-08-03 11:03:11 +0900

    feat: const type* const Function(const type* _type)

--- Coding/TEST/TEST/Smart_Pointer.cpp
+++ Coding/TEST/TEST/Smart_Pointer.cpp
@@ -9,8 +9,12 @@ public:
 	SimpleCat();			// Constructor
 	SimpleCat(SimpleCat& simplecat);		//Copy constructor
 	~SimpleCat();		//destructor
+	int GetAge() const { return itsAge; }
+	void SetAge(int age) { itsAge = age; }
+private:
+	int itsAge;
 };
-SimpleCat::SimpleCat()
+SimpleCat::SimpleCat() : itsAge(1)
 {
 	cout << "Simple Cat Constructor..\n";
 }
@@ -22,29 +26,38 @@ SimpleCat::~SimpleCat()
 {
 	cout << "Simple Cat Destructor..\n";
 }
-SimpleCat FunctionOne(SimpleCat theCat);
-SimpleCat* FunctionTwo(SimpleCat* theCat);
+const SimpleCat* const FunctionTwo(const SimpleCat*const theCat);
 int main()
 {
-	cout << "Making a cat...\n";
-	SimpleCat frisky;
-	cout << "Calling FunctionOne.. \n";
-	FunctionOne(frisky);
-	cout << "Calling FunctionTwo...\n";
-	FunctionTwo(&frisky);
+	cout << "Making a cat..\n";
+	SimpleCat Frisky;
+	cout << "Frisky is ";
+	cout << Frisky.GetAge();
+	cout << " years old\n";
+	cout << "-------------------\n";
+
+	int age = 5;
+	Frisky.SetAge(age);
+	cout << "Frisky is ";
+	cout << Frisky.GetAge();
+	cout << " year old\n";
+	cout << "-------------------\n";
+	cout << "Calling FunctionTwo..\n";
+	FunctionTwo(&Frisky);
+	cout << "-------------------\n";
+	cout << "Frisky is ";
+	cout << Frisky.GetAge();
+	cout << " years old\n";
+
 
 	return 0;
 }
-// FunctionOne, passes by value
-SimpleCat FunctionOne(SimpleCat theCat)
-{
-	cout << "Function One. Returning..\n";
-	return theCat;
-}
-
 // FunctionTwo, passes by reference
-SimpleCat* FunctionTwo(SimpleCat* theCat)
+const SimpleCat* const  FunctionTwo(const SimpleCat*const theCat)
 {
 	cout << "Function Two. Returning..\n";
+	cout << "Frisky is now " << theCat->GetAge();
+	cout << " years old \n";
+	// theCat->SetAge(6); const !
 	return theCat;
 }
