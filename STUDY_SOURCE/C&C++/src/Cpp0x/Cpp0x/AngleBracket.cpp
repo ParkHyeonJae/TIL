3218563105da09a0a5b187502b4090f451ee05f2
박현재 박 <md101106@gmail.com>
2021-02-19 19:04:37 +0900

    C++0x : Initializer List

---
+++ STUDY_SOURCE/C&C++/src/Cpp0x/Cpp0x/AngleBracket.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+/*
+C++0x란 C++언어의 다음 버전에 대한 가명칭을 말한다.
+VC++ 2008 : C++03
+C++0x : 200x년도
+프로그래밍을 간단하게 구현하기 위한 문법 추가와 기존 기능이 강화됨
+C++0x는 흔히 모던C++이라고 부르는 C++11로 바뀌게 된다.
+C++11부터 Lambda, Rvalue Reference 등 큰 변화가 도입되었다.
+*/
+
+int main(void)
+{
+
+	/*
+	기존에는 아래와 같으면 산괄호가 operator>> 로 해석되서 std::vector<std::basic_string<char> > 이런식으로 사이에
+	공백을 넣어줘야 했었는데 그럴 필요가 없어졌다.
+	*/
+	std::vector<std::basic_string<char>> v;
+
+
+	
+	return 0;
+}
---
+++ STUDY_SOURCE/C&C++/src/Cpp0x/Cpp0x/InitializerList.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <numeric>
+
+std::vector<int> vec()
+{
+	return { 3,2,2 };
+}
+
+void Foo(const std::vector<int>& v) {
+	std::cout << std::accumulate(v.begin(), v.end(), 0) << "\n";
+}
+
+class Vector3
+{
+public:
+
+	Vector3(std::initializer_list<int> list)
+	{
+		auto iter = list.begin();
+		x = *iter;
+		y = *(iter + 1);
+		z = *(iter + 2);
+	}
+
+	Vector3() = default;
+
+	void Print() const
+	{
+		std::cout << "X : " << x 
+			<< ", Y : " << y 
+			<< ", Z : " << z << "\n";
+	} 
+private:
+	int x, y, z;
+};
+
+int main(void)
+{
+	// Initalizer List
+
+	std::vector<int> v = { 1,2,3 };
+
+	Foo({ 2,2,2,2,2,2,2,2,2,2 });
+
+
+	std::map<std::string, int> mapTest = { {"Apple",123 }, {"Samsung", 345} ,{"ETC..", 678} };
+	for (auto& iter : mapTest)
+	{
+		std::cout << iter.first << " : " << iter.second << "\n";
+	}
+
+	Vector3 vec = { 1,2,3 };
+	vec.Print();
+
+	int arr[3] = { 5,6,7 };
+	for (auto& iter : arr)
+	{
+		std::cout << iter << "\n";
+	}
+	return 0;
+}
