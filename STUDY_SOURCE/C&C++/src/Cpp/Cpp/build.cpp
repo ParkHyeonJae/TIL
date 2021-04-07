91da73d69cf72e5806af3a61e0754fb41b624132
박현재 박 <md101106@gmail.com>
2021-04-08 01:30:41 +0900

    Build For g++

---
+++ STUDY_SOURCE/C&C++/src/Cpp/Cpp/build.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <variant>
+
+/*
+
+빌드
+g++ build.cpp -> a.exe
+
+g++ build.cpp -o greeting       -> greeting.exe
+
+g++ build.cpp -o greeting -Wall -> greeting.exe, Warning Check
+
+g++ build.cpp -o greeting -Wall -Werror -> greeting.exe, Warninig 발생시 빌드 X
+
+*/
+
+int main(void)
+{
+    int i = 0;      // not Initalized
+    std::cout << "HI" << std::endl;
+
+    std::variant<int, float> ii = 0;        // C++17
+
+    
+    return 0;
+}
