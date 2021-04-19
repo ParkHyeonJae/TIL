c800c5721bc555463a7831fc820a5b48ce17be63
박현재 박 <md101106@gmail.com>
2021-04-19 14:26:18 +0900

    BinarySearch & Merge_Sorted_Array

--- STUDY_SOURCE/C&C++/src/mordernCpp/mordernCpp/FalseSharing.cpp
+++ STUDY_SOURCE/C&C++/src/mordernCpp/mordernCpp/FalseSharing.cpp
@@ -4,11 +4,12 @@
 #include <array>
 #include <memory>
 #include <chrono>
+#include <new>
 
 std::array<int, 4> sums{ 0, 0, 0, 0 };
 int main(void)
 {
-	std::vector<int> nums(10000);
+	std::vector<int> nums(10000000);
 	std::fill(nums.begin(), nums.end(), 1);
 
 	std::vector<std::future<int>> futures;
@@ -17,38 +18,37 @@ int main(void)
 
 	std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
 
-	for (size_t i = 0; i < sums.size(); i++)
-	{
-		const int unitPerSize = nums.size() / sums.size();
-		const int loop = (unitPerSize) * (i + 1);
-		for (int idx = i * unitPerSize; idx < loop; idx++)
-			sums[i] += nums[idx];
-	}
-
 	//for (size_t i = 0; i < sums.size(); i++)
 	//{
 	//	const int unitPerSize = nums.size() / sums.size();
 	//	const int loop = (unitPerSize) * (i + 1);
-
-	//	futures.emplace_back(std::async(std::launch::async, [&, i] {
-	//		int sum = 0;
-	//		for (int idx = i * unitPerSize; idx < loop; idx++)
-	//			sum += nums[idx];
-
-	//		return sum;
-	//	}));
-
+	//	for (int idx = i * unitPerSize; idx < loop; idx++)
+	//		sums[i] += nums[idx];
 	//}
 
+	for (size_t i = 0; i < sums.size(); i++)
+	{
+		const int unitPerSize = nums.size() / sums.size();
+		const int loop = (unitPerSize) * (i + 1);
 
-	//for (size_t i = 0; i < futures.size(); i++)
-	//{
-	//	sums[i] = futures[i].get();
-	//	std::cout << sums[i] << "\n";
-	//}
+		futures.emplace_back(std::async(std::launch::async, [&, i] {
+			int sum = 0;
+			for (int idx = i * unitPerSize; idx < loop; idx++)
+				sum += nums[idx];
+			return sum;
+		}));
 
-	for (auto& v : sums)
-		std::cout << v << "\n";
+	}
+
+
+	for (size_t i = 0; i < futures.size(); i++)
+	{
+		sums[i] = futures[i].get();
+		std::cout << sums[i] << "\n";
+	}
+
+	//for (auto& v : sums)
+	//	std::cout << v << "\n";
 
 	std::chrono::system_clock::time_point end = std::chrono::system_clock::now();
 
