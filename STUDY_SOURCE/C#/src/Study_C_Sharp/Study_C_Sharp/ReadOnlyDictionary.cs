42070d1dbfe1d3acda9aa6c2a3f817737b5bd9b9
박현재 박 <md101106@gmail.com>
2021-05-23 20:10:23 +0900

    ReadOnly Dictionary

---
+++ STUDY_SOURCE/C#/src/Study_C_Sharp/Study_C_Sharp/ReadOnlyDictionary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Study_C_Sharp
+{
+    class ReadOnlyDictionary
+    {
+
+        static readonly Dictionary<Int32, String> readonlyDict = new Dictionary<int, string>()
+        {
+            {0, "안녕" },
+            {1, "반가워" },
+            {2, "나의 이름은" },
+            {3, "박현재야" },
+        };
+
+
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < readonlyDict.Count; i++)
+            {
+                Console.WriteLine(readonlyDict[i]);
+            }
+
+
+            foreach (var pair in readonlyDict)
+            {
+                Console.WriteLine(pair.Value);
+            }
+        }
+    }
+}
