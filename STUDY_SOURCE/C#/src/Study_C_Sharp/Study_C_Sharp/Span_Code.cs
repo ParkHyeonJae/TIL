44d7b85f7080e3f961c5169854ae1877d4ad0e20
박현재 박 <md101106@gmail.com>
2021-05-23 20:06:41 +0900

    Span<T> & ReadOnlySpan<T> & stackalloc

---
+++ STUDY_SOURCE/C#/src/Study_C_Sharp/Study_C_Sharp/Span_Code.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Study_C_Sharp
+{
+    class Span_Code
+    {
+        //unsafe public static void CallByPointer(int* ptr)
+        //{
+        //    ptr[5] = 200;
+        //}
+
+
+        //unsafe static void Main(string[] args)
+        //{
+
+
+
+        //    int[] t = new int[256];
+
+        //    fixed (int* t1 = &t[0])
+        //    {
+        //        CallByPointer(t1);
+        //    }
+
+        //    Console.WriteLine(t[5]);
+
+        //}
+
+
+        unsafe public static void CallByPointer(int* ptr)
+        {
+            ptr[5] = 20;
+        }
+        public static void CallByPointer(Span<int> ptr)
+        {
+            ptr[5] = 20;
+        }
+        public static void CallByPointer(int[] ptr)
+        {
+            ptr[5] = 20;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+            
+            Span<int> spanArr = stackalloc int[256];
+            ReadOnlySpan<char> st = stackalloc char[] { 'h', 'a' };
+
+
+            
+            //unsafe
+            //{
+            //    fixed (int* a = &spanArr[0])
+            //    {
+            //        CallByPointer(a);
+            //    }
+            //}
+
+            CallByPointer(spanArr);
+
+            Console.WriteLine(spanArr[5]);          // 20
+
+        }
+
+    }
+}
---
+++ STUDY_SOURCE/C#/src/Study_C_Sharp/Study_C_Sharp/UnSafe_Code.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.CompilerServices;
+
+namespace Study_C_Sharp
+{
+    class UnSafe_Code
+    {
+
+
+        //unsafe static void Main(string[] args)
+        //{
+        //    const int Length = 1500;
+        //    int* a = stackalloc int[Length];
+        //    for (int i = 0; i < Length; i++)
+        //        a[i] = i;
+
+        //    for (int i = 0; i < Length; i++)
+        //    {
+        //        Console.WriteLine(a[i]);
+        //    }
+
+
+        //}
+
+        class Point
+        {
+            public int x;
+            public int y;
+
+        }
+
+        unsafe static void CallByPointer(int* ptr)
+        {
+            *ptr = 10;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Point point = new Point();
+            point.x = 200;
+
+            unsafe
+            {
+
+                fixed(int* x = &point.x)
+                {
+                    CallByPointer(x);
+                }
+
+
+            }
+
+
+            Console.WriteLine(point.x);
+
+
+        }
+
+
+    }
+}
